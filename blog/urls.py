d8cb8080bba262c32e0fb56d28c840f17453a5ea
Thanos Dimitriades <thanosngplus@gmail.com>
2023-11-08 22:21:20 +0200

    feat: add CRUD for blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogDeleteView, BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 
 urlpatterns = [
-    # I don't think exposing the primary key is safe...
-    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"), # unsafe for db security
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
     path("", BlogListView.as_view(), name="home")
 ]
